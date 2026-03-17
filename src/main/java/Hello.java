b10b76953c5b9341556c79fc3977a1954def38dd
yunee1113-glitch <yunee1113@gmail.com>
2026-03-17 15:31:36 +0900

    초기 커밋

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,3 +1,6 @@
-public class Hello{ public static void main(String[] args) {
-    System.out.println("Hello!! My First Java Coding!!");}System.out.println("Edit Java Code!!");}
+public class Hello {
+public static void main(String[] args){
+       System.out.println("Hello!! My First Java Coding!!");
+       System.out.println("Edit Java Code!!");}}
+
 
--- src/main/java/hello2.java
+++ src/main/java/hello2.java
@@ -1,2 +1,5 @@
 public class hello2 {
+public static void main(String[] args){
+    System.out.println("안녕하세요!");}
 }
+
