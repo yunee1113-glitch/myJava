755309f945bafa2369bebfce11475dec54e03cec
yunee1113-glitch <yunee1113@gmail.com>
2026-03-18 11:25:27 +0900

    초기 커밋

---
+++ src/main/java/Code02_10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Code02_10 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int num;
+
+        num = s.nextInt();
+        System.out.println("사용자가 입력한 값 ==> " + num);
+
+        s.close();
+    }
+}
