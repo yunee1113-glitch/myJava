c3c2a06156a2855009a12e4883ff068e9d2e0de5
yunee1113-glitch <yunee1113@gmail.com>
2026-04-10 15:23:41 +0900

    초기 커밋

---
+++ src/main/java/Code06_08.java
@@ -0,0 +1,9 @@
+public class Code06_08 {
+    public static void main(String[]args) {
+        int hap = 0;
+        for (int i=1001 ; i<=2000 ; i+=2) {
+            hap += i;
+        }
+        System.out.println("1000부터 2000까지 홀수의 합계 : " + hap);
+    }
+}
