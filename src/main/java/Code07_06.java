96b0640c576313a861e1b07fce5819ae292336c6
yunee1113-glitch <yunee1113@gmail.com>
2026-04-17 15:44:08 +0900

    초기 커밋

--- src/main/java/Code07_06.java
+++ src/main/java/Code07_06.java
@@ -24,13 +24,13 @@ class Rabbit {
         return this.yPos;
     }
 }
-    public class Code07_06 {
-        public static void main(String[] args) {
-            Rabbit rabbit = new Rabbit();
-            rabbit.setShape("흰토끼");
-            rabbit.setPosition(100, 100);
+public class Code07_06 {
+    public static void main(String[] args) {
+        Rabbit rabbit = new Rabbit();
+        rabbit.setShape("흰토끼");
+        rabbit.setPosition(100, 100);
 
-            System.out.printf("토끼의 모양은 [%s]입니다.\n", rabbit.getShape());
-            System.out.printf("토끼의 위치는 (%d, %d)입니다.\n", rabbit.getX(), rabbit.getY());
-        }
+        System.out.printf("토끼의 모양은 [%s]입니다.\n", rabbit.getShape());
+        System.out.printf("토끼의 위치는 (%d, %d)입니다.\n", rabbit.getX(), rabbit.getY());
     }
+}
